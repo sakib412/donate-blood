48433cf8d1292b0c656bb5e6f59d465723e569f4
Najmus Sakib <nazmusakib412@gmail.com>
2023-05-02 11:41:03 +0600

    project initialized

---
+++ donate-blood.php
@@ -0,0 +1,23 @@
+/**
+* Donate blood
+*
+* @package DonateBlood
+* @author Najmus Sakib
+* @copyright 2023 Authlab
+* @license GPL-2.0-or-later
+*
+* @wordpress-plugin
+* Plugin Name: Donate blood
+* Plugin URI: https://github.com/sakib412/donate-blood
+* Description: Create a form for blood donation
+* Version: 0.0.1
+* Requires at least: 5.2
+* Requires PHP: 7.2
+* Author: Najmus Sakib
+* Author URI: https://github.com/sakib412
+* License: GPL v2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Update URI: https://github.com/sakib412/donate-blood
+* Text Domain: donate_blood
+* Domain Path: /languages
+*/
